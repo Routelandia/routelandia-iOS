d5fcbefcc58db3ed40d55c4b09a8db341d5e72ce
Josh Proehl <josh@daedalusdreams.com>
2015-03-21 19:17:19 -0700

    Add spinners to date picker

--- Routelandia/TimePickerViewController.swift
+++ Routelandia/TimePickerViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 import MapKit
 
-class TimePickerViewController: UIViewController {
+class TimePickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
+  
+  @IBOutlet weak var dayPicker: UIPickerView!
+  @IBOutlet weak var timePicker: UIDatePicker!
+  
+  
+  let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  
   required init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  /** UIPickerView Data Source and Delegates **/
+  // returns the number of 'columns' to display.
+  func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  // returns the # of rows in each component..
+  func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return 7
+  }
+  
+  // Show the value for the requested row
+  func pickerView(_: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+    return daysOfWeek[row]
   }
 }
