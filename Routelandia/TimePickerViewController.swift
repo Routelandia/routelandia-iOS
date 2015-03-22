facdcb63dbbd7a63d79e74127f3428fbb8e2c94f
Josh Proehl <josh@daedalusdreams.com>
2015-03-21 17:07:18 -0700

    Add second and third views

---
+++ Routelandia/TimePickerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TimePickerViewController.swift
+//  Routelandia
+//
+//  Created by Josh Proehl on 3/21/15.
+//  Copyright (c) 2015 Portland State University. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TimePickerViewController: UIViewController {
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
