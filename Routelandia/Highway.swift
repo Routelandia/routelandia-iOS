c2c12d813fd31d83c04d9f163c5d6e8ba7dd8bf7
Peter Gicking <pgicking@gmail.com>
2015-03-28 23:50:45 -0700

    Add Highway/Highways. (Squashed commits from failed cocoapods branch)

---
+++ Routelandia/Highway.swift
@@ -0,0 +1,31 @@
+//
+//  Highway.swift
+//  Routelandia
+//
+//  Created by Peter Gicking on 3/24/15.
+//  Copyright (c) 2015 Portland State University. All rights reserved.
+//
+
+import Foundation
+
+
+class Highway{
+    let highwayid: Int32
+    let direction: String
+    let highwayname: String
+    let bound: String
+    let oppositehighwayid: Int32
+    
+    init(json:JSON){
+        highwayid = json["results"][0]["highwayid"].int32!
+        direction = json["results"][0]["direction"].stringValue
+        highwayname = json["results"][0]["highwayname"].stringValue
+        bound = json["results"][0]["bound"].stringValue
+        oppositehighwayid = json["results"][0]["oppositehighwayid"].int32!
+    }
+    
+    
+}
+
+    
+    
