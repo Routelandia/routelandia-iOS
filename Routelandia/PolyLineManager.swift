6ae05ef9cdd76e555e984c3b0cb1bc9270fb9573
pgicking <pgicking@gmail.com>
2015-04-01 19:08:30 -0700

    created polylineManger class

---
+++ Routelandia/PolyLineManager.swift
@@ -0,0 +1,33 @@
+//
+//  PolyLineManager.swift
+//  Routelandia
+//
+//  Created by Peter Gicking on 4/1/15.
+//  Copyright (c) 2015 Portland State University. All rights reserved.
+//
+
+import Foundation
+
+class PolyLineManager{
+    
+    func getAllhighways(){
+        let highways = ApiFetcher().fetchJson("highways")
+        
+        //dynamically get number of highways
+        //pass highway ids to different function
+            //grab all the coordinates IN ORDER
+            //return that polyline
+        //grab polyline, add it to list
+        //return list to map view
+        
+        var i = 0
+        var highwayIds = [Int32]()
+        while(highways["results"][i] == nil){
+            highwayIds.append(highways["results"][i]["highwayid"].int32!)
+            i = i + 1
+        }
+        
+        
+    }
+    
+}
